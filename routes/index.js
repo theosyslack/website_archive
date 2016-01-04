232f2d5f54548c0a3c847dc5bb6ece0ef59415cb
Theo Syslack <theosyslack@gmail.com>
2016-01-03 22:29:37 -0600

    Unescape description content

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,19 @@ router.get('/', function(req, res) {
   var data = {
     title: 'Theo Syslack',
     subtitle: 'Web Developer & World Famous Detective',
+    employeementStatus: {
+        status: 'Employed',
+        location: 'Ice Frog Technologies'
+    },
     projects: [
       {
         title: 'RatingsMax',
         subtitle: 'Online review management for small businesses.',
         description: 'Over the course of 7 months, our development team'+
          ' of three built an entire web app to help small businesses'+
-         ' manage their online reputation.',
+         ' manage their online reputation. Using <a href="https://laravel.com/">Laravel</a>,'+
+         ' we built a fast and functional app using'+
+         ' the latest standards in the PHP community.',
         images: [
           {
             src: '/img/project-images/ratingsmax/reviews.png',
@@ -38,48 +44,8 @@ router.get('/', function(req, res) {
           },
           {
             title: 'Middle End',
-            skills: ['Controllers', 'Repositories', 'Services', 'Blade']
+            skills: ['PHP', 'Controllers', 'Repositories', 'Services', 'Blade']
           }
-
-        ]
-      },
-      {
-        title: 'RatingsMax',
-        subtitle: 'Online review management for small businesses.',
-        description: 'Over the course of 7 months, our development team'+
-         ' of three built an entire web app to help small businesses'+
-         ' manage their online reputation.',
-        images: [
-          {
-            src: '/img/project-images/ratingsmax/reviews.png',
-            alt: 'Image',
-            classes: '-left -full'
-          },
-          {
-            src: '/img/project-images/ratingsmax/settings.png',
-            alt: 'Image2',
-            classes: '-right -half'
-          },
-          {
-            src: '/img/project-images/ratingsmax/sharing.png',
-            alt: 'Image3',
-            classes: '-right -half'
-          }
-        ],
-        skillSets: [
-          {
-            title: 'Font End',
-            skills: ['Sass', 'Gulp', 'Flexbox']
-          },
-          {
-            title: 'Middle End',
-            skills: ['Controllers', 'Repositories', 'Services', 'Blade']
-          },
-          {
-            title: 'Back End',
-            skills: ['Laravel', 'Gulp', 'Flexbox']
-          }
-
         ]
       }
     ]
