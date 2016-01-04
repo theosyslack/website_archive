f9af7c2b9fb05f9eb854d810a979440e2d3c701b
Theo Syslack <theosyslack@gmail.com>
2016-01-03 22:09:44 -0600

    Added Skill Sets

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res) {
     projects: [
       {
         title: 'RatingsMax',
-        subtitle: 'RatingsMax',
+        subtitle: 'Online review management for small businesses.',
         description: 'Over the course of 7 months, our development team'+
          ' of three built an entire web app to help small businesses'+
          ' manage their online reputation.',
@@ -30,6 +30,56 @@ router.get('/', function(req, res) {
             alt: 'Image3',
             classes: '-right -half'
           }
+        ],
+        skillSets: [
+          {
+            title: 'Font End',
+            skills: ['Sass', 'Gulp', 'Flexbox']
+          },
+          {
+            title: 'Middle End',
+            skills: ['Controllers', 'Repositories', 'Services', 'Blade']
+          }
+
+        ]
+      },
+      {
+        title: 'RatingsMax',
+        subtitle: 'Online review management for small businesses.',
+        description: 'Over the course of 7 months, our development team'+
+         ' of three built an entire web app to help small businesses'+
+         ' manage their online reputation.',
+        images: [
+          {
+            src: '/img/project-images/ratingsmax/reviews.png',
+            alt: 'Image',
+            classes: '-left -full'
+          },
+          {
+            src: '/img/project-images/ratingsmax/settings.png',
+            alt: 'Image2',
+            classes: '-right -half'
+          },
+          {
+            src: '/img/project-images/ratingsmax/sharing.png',
+            alt: 'Image3',
+            classes: '-right -half'
+          }
+        ],
+        skillSets: [
+          {
+            title: 'Font End',
+            skills: ['Sass', 'Gulp', 'Flexbox']
+          },
+          {
+            title: 'Middle End',
+            skills: ['Controllers', 'Repositories', 'Services', 'Blade']
+          },
+          {
+            title: 'Back End',
+            skills: ['Laravel', 'Gulp', 'Flexbox']
+          }
+
         ]
       }
     ]
